ee5bbbf8d814f0f6a5582257bb33362baf0af71a
Kendrick Calata <kcalata@email.arizona.edu>
2019-04-03 19:57:34 -0400

    Find top 3 hits through Log table

--- LogsAnalysis.py
+++ LogsAnalysis.py
@@ -1,1 +1,8 @@
 import psychopg2
+
+db = psycopg2.connect("dbname=news")
+c = db.cursor()
+c.execute('select path, count(*) as num from log group by oath order by num desc limit 3 offset 1;')
+results = cursor.fetchall()
+print results
+db.close()
