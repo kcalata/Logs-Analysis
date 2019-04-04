8e15b8935d8031c14c5d1f43217ec0e829f0937d
Kendrick Calata <kcalata@email.arizona.edu>
2019-04-03 20:23:27 -0400

    Fix formatting

--- LogsAnalysis.py
+++ LogsAnalysis.py
@@ -1,8 +1,14 @@
-import psychopg2
+import psycopg2
 
 db = psycopg2.connect("dbname=news")
 c = db.cursor()
-c.execute("select articles.title, count(*) as views from log, articles where log.status='200 OK' and articles.slug = substr(log.path, 10) group by articles.title order by views desc limit 3;")
-results = cursor.fetchall()
+c.execute("""select articles.title, count(*) as views
+from log, articles
+where log.status='200 OK'
+and articles.slug = substr(log.path, 10)
+group by articles.title
+order by views desc
+limit 3;""")
+results = c.fetchall()
 print results
 db.close()
