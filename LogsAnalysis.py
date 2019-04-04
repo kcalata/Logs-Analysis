66ebf92898034bd2091839ba6d1a2315109cc69e
Kendrick Calata <kcalata@email.arizona.edu>
2019-04-04 01:25:24 -0400

    Add report function to print to output file

--- LogsAnalysis.py
+++ LogsAnalysis.py
@@ -1,40 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import psycopg2
+import os
 
-db = psycopg2.connect("dbname=news")
-c = db.cursor()
-c.execute("""select articles.title, count(*) as views
-from log, articles
-where log.status = '200 OK'
-and articles.slug = substr(log.path, 10)
-group by articles.title
-order by views desc
-limit 3;""")
-results = c.fetchall()
-print results
-db.close()
+def sort_articles():
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    c.execute("""select articles.title, count(*) as views
+    from log, articles
+    where log.status = '200 OK'
+    and articles.slug = substr(log.path, 10)
+    group by articles.title
+    order by views desc
+    limit 3;""")
+    results = c.fetchall()
+    db.close()
+    return results
 
-db = psycopg2.connect("dbname=news")
-c = db.cursor()
-c.execute("""select authors.name, count(*) as views
-from log, authors, articles
-where log.status = '200 OK'
-and articles.slug = substr(log.path, 10)
-and authors.id = articles.author
-group by authors.name
-order by views desc;""")
-results = c.fetchall()
-print results
-db.close()
+def sort_authors():
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    c.execute("""select authors.name, count(*) as views
+    from log, authors, articles
+    where log.status = '200 OK'
+    and articles.slug = substr(log.path, 10)
+    and authors.id = articles.author
+    group by authors.name
+    order by views desc;""")
+    results = c.fetchall()
+    db.close()
+    return results
 
-db = psycopg2.connect("dbname=news")
-c = db.cursor()
-c.execute()"""select errors.date,
-errors.err/errors.total as percentage
-from (select cast(time as date) as date,
-count(*) as total,
-cast(sum(cast(status != '200 OK' as int)) as float) as err
-from log group by date) as errors
-where errors.err/errors.total > 0.01;""")
-results = c.fetchall()
-print results
-db.close()
+def errors():
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    c.execute("""select errors.date,
+    errors.err/errors.total * 100 as percentage
+    from (select cast(time as date) as date,
+    count(*) as total,
+    cast(sum(cast(status != '200 OK' as int)) as float) as err
+    from log group by date) as errors
+    where errors.err/errors.total > 0.01;""")
+    results = c.fetchall()
+    db.close()
+    return results
+
+def report():
+    questions = ['1. What are the most popular three articles of all time?\n',
+    '2. Who are the most popular article authors of all time?\n',
+    '3. On which days did more than 1% of requests lead to errors?\n']
+    results = [sort_articles(), sort_authors(), errors()]
+    f = open('LogsAnalysis.txt', 'w')
+    f.write(questions[0])
+    f.write('• \"' + str(results[0][0][0]) + '\" - ' + str(results[0][0][1]) + ' views\n')
+    f.write('• \"' + str(results[0][1][0]) + '\" - ' + str(results[0][1][1]) + ' views\n')
+    f.write('• \"' + str(results[0][2][0]) + '\" - ' + str(results[0][2][1]) + ' views\n\n')
+    f.write(questions[1])
+    f.write('• ' + str(results[1][0][0]) + ' - ' + str(results[1][0][1]) + ' views\n')
+    f.write('• ' + str(results[1][1][0]) + ' - ' + str(results[1][1][1]) + ' views\n')
+    f.write('• ' + str(results[1][2][0]) + ' - ' + str(results[1][2][1]) + ' views\n\n')
+    f.write(questions[2])
+    f.write('• ' + str(results[2][0][0]) + ' - ' + str(results[2][0][1]) + '% errors\n')
+    f.close()
+
+if __name__ == "__main__":
+    report()
