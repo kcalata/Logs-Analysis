f547623d5201b3512d4593331fd395da2ba79b15
Kendrick Calata <kcalata@email.arizona.edu>
2019-04-03 20:32:20 -0400

    Show author and views in descending order of views

--- LogsAnalysis.py
+++ LogsAnalysis.py
@@ -4,11 +4,24 @@ db = psycopg2.connect("dbname=news")
 c = db.cursor()
 c.execute("""select articles.title, count(*) as views
 from log, articles
-where log.status='200 OK'
+where log.status = '200 OK'
 and articles.slug = substr(log.path, 10)
 group by articles.title
 order by views desc
 limit 3;""")
+results = c.fetchall()
+print results
+db.close()
+
+db = psycopg2.connect("dbname=news")
+c = db.cursor()
+c.execute("""select authors.name, count(*) as views
+from log, authors, articles
+where log.status = '200 OK'
+and articles.slug = substr(log.path, 10)
+and authors.id = articles.author
+group by authors.name
+order by views desc;""")
 results = c.fetchall()
 print results
 db.close()
